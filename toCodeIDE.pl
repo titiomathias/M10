cf0b212238e09fe2e8d18a3e3f5a933026ef0cb4
Matheus de Alencar Costa Oliveira <matheus.alencar@alunos.ifsuldeminas.edu.br>
2023-10-19 10:20:16 -0300

    My first prolog database

---
+++ toCodeIDE.pl
@@ -0,0 +1,31 @@
+linguagemDeProgramacao(python).
+linguagemDeProgramacao(c).
+linguagemDeProgramacao(java).
+linguagemDeProgramacao(php).
+linguagemDeProgramacao(ruby).
+
+linguagemDeMarcacao(html).
+linguagemDeMarcacao(xml).
+linguagemDeMarcacao(xhtml).
+linguagemDeMarcacao(css).
+
+ide(vscode, html).
+ide(vscode, xml).
+ide(vscode, xhtml).
+ide(vscode, css).
+ide(sublime, html).
+ide(sublime, css).
+ide(sublime, xhtml).
+ide(sublime, xml).
+ide(sublime, php).
+ide(pycharm, python).
+ide(intellij, java).
+ide(codeblocks, c).
+
+poo(X, Y) :-
+    ide(X, Y),
+    linguagemDeProgramacao(Y).
+
+marcacao(X, Y) :-
+    ide(X, Y),
+    linguagemDeMarcacao(Y).
