88b20e0a879651fe2e4b07acf597abf553f05ba5
Matheus de Alencar Costa Oliveira <matheus.alencar@alunos.ifsuldeminas.edu.br>
2023-10-25 14:43:31 -0300

    new prolog project
    based in recurrence concept

---
+++ countryStateCity/countryStateCity.pl
@@ -0,0 +1,406 @@
+country(albania).
+country(algeria).
+country(angola).
+country(belgium).
+country(dominican_republic).
+country(ecuador).
+country(egypt).
+country(ethiopia).
+country(finland).
+country(germany).
+country(greenland).
+country(guadeloupe).
+country(haiti).
+country(india).
+country(iran).
+country(iraq).
+country(israel).
+country(italy).
+country(japan).
+country(jersey).
+country(korea_north).
+country(korea_south).
+country(madagascar).
+country(mexico).
+country(mongolia).
+country(mozambique).
+country(namibia).
+country(new_zealand).
+country(nicaragua).
+country(pakistan).
+country(romania).
+country(russia).
+country(serbia).
+country(singapore).
+country(slovenia).
+country(spain).
+country(tunisia).
+country(turkey).
+country(united_states).
+
+thestate(albania, berat).
+thestate(albania, bulqize).
+thestate(albania, delvine).
+
+thestate(algeria, ayn_daflah).
+thestate(algeria, ayn_tamushanat).
+thestate(algeria, adrar).
+
+thestate(angola, bengo).
+thestate(angola, benguela).
+thestate(angola, bie).
+
+thestate(belgium, antwerpen).
+thestate(belgium, berchem).
+thestate(belgium, brabant).
+
+thestate(dominican_republic, azua).
+thestate(dominican_republic, bahoruco).
+thestate(dominican_republic, barahona).
+
+thestate(ecuador, azuay).
+thestate(ecuador, bolivar).
+thestate(ecuador, canar).
+
+thestate(egypt, ad-daqahliyah).
+thestate(egypt, al-bahr-al-ahmar).
+thestate(egypt, al-buhayrah).
+
+thestate(ethiopia, addis_abeba).
+thestate(ethiopia, afar).
+thestate(ethiopia, amhara).
+
+thestate(finland, ahvenanmaa).
+thestate(finland, etela-karjala).
+thestate(finland, etela-pohjanmaa).
+
+thestate(germany, auvergne).
+thestate(germany, baden-wurttemberg).
+thestate(germany, bavaria).
+
+thestate(greenland, aasiaat).
+thestate(greenland, ammassalik).
+thestate(greenland, illoqqortoormiut).
+
+thestate(guadeloupe, basse-terre).
+thestate(guadeloupe, grande-terre).
+thestate(guadeloupe, iles_des_saintes).
+
+thestate(haiti, artibonite).
+thestate(haiti, centre).
+thestate(haiti, grandanse).
+
+thestate(india, andaman_and_nicobar_islands).
+thestate(india, andhra_pradesh).
+thestate(india, arunachal_pradesh).
+
+thestate(iran, ardabil).
+thestate(iran, azarbayjan-e_bakhtari).
+thestate(iran, azarbayjan-e_khavari).
+
+thestate(iraq, al-anbar).
+thestate(iraq, al-basrah).
+thestate(iraq, al-muthanna).
+
+thestate(israel, beit_hanania).
+thestate(israel, ben_gurion_airport).
+thestate(israel, bethlehem).
+
+thestate(italy, abruzzi).
+thestate(italy, abruzzo).
+thestate(italy, agrigento).
+
+thestate(japan, aichi).
+thestate(japan, akita).
+thestate(japan, aomori).
+
+thestate(jersey, grouville).
+thestate(jersey, saint_brelade).
+thestate(jersey, saint_clement).
+
+thestate(korea_north, chagangdo).
+thestate(korea_north, hamgyeongbukto).
+thestate(korea_north, hamgyeongnamdo).
+
+thestate(korea_south, busan).
+thestate(korea_south, cheju).
+thestate(korea_south, chollabuk).
+
+thestate(madagascar, antananarivo).
+thestate(madagascar, antsiranana).
+thestate(madagascar, fianarantsoa).
+
+thestate(mexico, aguascalientes).
+thestate(mexico, baja_california).
+thestate(mexico, baja_california_sur).
+
+thestate(mongolia, arhangaj).
+thestate(mongolia, bajan-olgij).
+thestate(mongolia, bajanhongor).
+
+thestate(mozambique, cabo_delgado).
+thestate(mozambique, gaza).
+thestate(mozambique, inhambane).
+
+thestate(namibia, caprivi).
+thestate(namibia, erongo).
+thestate(namibia, hardap).
+
+thestate(new_zealand, area_outside_region).
+thestate(new_zealand, auckland).
+thestate(new_zealand, bay_of_plenty).
+
+thestate(pakistan, baluchistan).
+thestate(pakistan, federal_capital_area).
+thestate(pakistan, federally_administered_tribal_).
+
+thestate(romania, alba).
+thestate(romania, arad).
+thestate(romania, arges).
+
+thestate(russia, adygeja).
+thestate(russia, aga).
+thestate(russia, alanija).
+
+thestate(serbia, central_serbia).
+thestate(serbia, kosovo_and_metohija).
+thestate(serbia, vojvodina).
+
+thestate(singapore, singapore).
+
+thestate(slovenia, benedikt).
+thestate(slovenia, gorenjska).
+thestate(slovenia, gorishka).
+
+thestate(spain, a_coruna).
+thestate(spain, alacant).
+thestate(spain, alava).
+
+thestate(tunisia, al-kaf).
+thestate(tunisia, al-mahdiyah).
+thestate(tunisia, al-munastir).
+
+thestate(turkey, adana).
+thestate(turkey, adiyaman).
+thestate(turkey, afyon).
+
+thestate(united_states, alabama).
+thestate(united_states, alaska).
+thestate(united_states, arizona).
+                   
+city(berat).
+city(bulqize).
+city(delvine).
+
+city(ayn_daflah).
+city(ayn_tamushanat).
+city(adrar).
+
+city(bengo).
+city(benguela).
+city(bie).
+
+city(antwerpen).
+city(berchem).
+city(brabant).
+
+city(azua).
+city(bahoruco).
+city(barahona).
+
+city(azuay).
+city(bolivar).
+city(canar).
+
+city(ad-daqahliyah).
+city(al-bahr-al-ahmar).
+city(al-buhayrah).
+
+city(addis_abeba).
+city(afar).
+city(amhara).
+
+city(ahvenanmaa).
+city(etela-karjala).
+city(etela-pohjanmaa).
+
+city(auvergne).
+city(baden-wurttemberg).
+city(bavaria).
+
+city(aasiaat).
+city(ammassalik).
+city(illoqqortoormiut).
+
+city(basse-terre).
+city(grande-terre).
+city(iles_des_saintes).
+
+city(artibonite).
+city(centre).
+city(grandanse).
+
+city(andaman_and_nicobar_islands).
+city(andhra_pradesh).
+city(arunachal_pradesh).
+
+city(ardabil).
+city(azarbayjan-e_bakhtari).
+city(azarbayjan-e_khavari).
+
+city(al-anbar).
+city(al-basrah).
+city(al-muthanna).
+
+city(beit_hanania).
+city(ben_gurion_airport).
+city(bethlehem).
+
+city(abruzzi).
+city(abruzzo).
+city(agrigento).
+
+city(aichi).
+city(akita).
+city(aomori).
+
+city(grouville).
+city(saint_brelade).
+city(saint_clement).
+
+city(chagangdo).
+city(hamgyeongbukto).
+city(hamgyeongnamdo).
+
+city(busan).
+city(cheju).
+city(chollabuk).
+
+city(antananarivo).
+city(antsiranana).
+city(fianar).
+
+inside(albania, berat).
+inside(albania, bulqize).
+inside(albania, delvine).
+inside(algeria, ayn_daflah).
+inside(algeria, ayn_tamushanat).
+inside(algeria, adrar).
+inside(angola, bengo).
+inside(angola, benguela).
+inside(angola, bie).
+inside(belgium, antwerpen).
+inside(belgium, berchem).
+inside(belgium, brabant).
+inside(dominican_republic, azua).
+inside(dominican_republic, bahoruco).
+inside(dominican_republic, barahona).
+inside(ecuador, azuay).
+inside(ecuador, bolivar).
+inside(ecuador, canar).
+inside(egypt, ad-daqahliyah).
+inside(egypt, al-bahr-al-ahmar).
+inside(egypt, al-buhayrah).
+inside(ethiopia, addis_abeba).
+inside(ethiopia, afar).
+inside(ethiopia, amhara).
+inside(finland, ahvenanmaa).
+inside(finland, etela-karjala).
+inside(finland, etela-pohjanmaa).
+inside(germany, auvergne).
+inside(germany, baden-wurttemberg).
+inside(germany, bavaria).
+inside(greenland, aasiaat).
+inside(greenland, ammassalik).
+inside(greenland, illoqqortoormiut).
+inside(guadeloupe, basse-terre).
+inside(guadeloupe, grande-terre).
+inside(guadeloupe, iles_des_saintes).
+inside(haiti, artibonite).
+inside(haiti, centre).
+inside(haiti, grandanse).
+inside(india, andaman_and_nicobar_islands).
+inside(india, andhra_pradesh).
+inside(india, arunachal_pradesh).
+inside(iran, ardabil).
+inside(iran, azarbayjan-e_bakhtari).
+inside(iran, azarbayjan-e_khavari).
+inside(iraq, al-anbar).
+inside(iraq, al-basrah).
+inside(iraq, al-muthanna).
+inside(israel, beit_hanania).
+inside(israel, ben_gurion_airport).
+inside(israel, bethlehem).
+inside(italy, abruzzi).
+inside(italy, abruzzo).
+inside(italy, agrigento).
+inside(japan, aichi).
+inside(japan, akita).
+inside(japan, aomori).
+inside(jersey, grouville).
+inside(jersey, saint_brelade).
+inside(jersey, saint_clement).
+inside(korea_north, chagangdo).
+inside(korea_north, hamgyeongbukto).
+inside(korea_north, hamgyeongnamdo).
+inside(korea_south, busan).
+inside(korea_south, cheju).
+inside(korea_south, chollabuk).
+inside(madagascar, antananarivo).
+inside(madagascar, antsiranana).
+inside(madagascar, fianarantsoa).
+inside(mexico, aguascalientes).
+inside(mexico, baja_california).
+inside(mexico, baja_california_sur).
+inside(mongolia, arhangaj).
+inside(mongolia, bajan-olgij).
+inside(mongolia, bajanhongor).
+inside(mozambique, cabo_delgado).
+inside(mozambique, gaza).
+inside(mozambique, inhambane).
+inside(namibia, caprivi).
+inside(namibia, erongo).
+inside(namibia, hardap).
+inside(new_zealand, area_outside_region).
+inside(new_zealand, auckland).
+inside(new_zealand, bay_of_plenty).
+inside(pakistan, baluchistan).
+inside(pakistan, federal_capital_area).
+inside(pakistan, federally_administered_tribal_).
+inside(romania, alba).
+inside(romania, arad).
+inside(romania, arges).
+inside(russia, adygeja).
+inside(russia, aga).
+inside(russia, alanija).
+inside(serbia, central_serbia).
+inside(serbia, kosovo_and_metohija).
+inside(serbia, vojvodina).
+inside(singapore, singapore).
+inside(slovenia, benedikt).
+inside(slovenia, gorenjska).
+inside(slovenia, gorishka).
+inside(spain, a_coruna).
+inside(spain, alacant).
+inside(spain, alava).
+inside(tunisia, al-kaf).
+inside(tunisia, al-mahdiyah).
+inside(tunisia, al-munastir).
+inside(turkey, adana).
+inside(turkey, adiyaman).
+inside(turkey, afyon).
+inside(united_states, alabama).
+inside(united_states, alaska).
+inside(united_states, arizona).
+
+estado(X) :- thestate(_, X).
+
+territory(X, Y) :- inside(X, Y).
+territory(X, Y) :- inside(X, Z), territory(Z, Y).
+
+whichcountry(X, Y) :- inside(Y, X), country(Y).
+whichcountry(X, Y) :- territory(Y, X), country(Y).
+
+% territory(japan, X) -> Which is in database (country) territory?
+% whichcountry(aichi, X) -> From where is Aichi (country) ?
+% whichstate -> This is the posssible next step. (still don't developed)
---
+++ countryStateCity/searchCSC.py
@@ -0,0 +1,64 @@
+import requests
+
+auth_token = ("JSON WEB TOKEN")
+
+headers = {
+    "Authorization": auth_token,
+    "Accept": "application/json"
+}
+
+paises = ['Albania', 'Algeria', 'Angola', 'Belgium',
+          'Dominican Republic', 'Ecuador', 'Egypt','Ethiopia',
+          'Finland',
+          'Germany', 'Greenland', 'Guadeloupe',
+          'Haiti', 'India', 'Iran', 'Iraq', 'Israel', 'Italy', 'Japan', 'Jersey', 'Korea North',
+          'Korea South',
+          'Madagascar',
+          'Mexico', 'Mongolia', 'Mozambique', 'Namibia',
+          'New Zealand', 'Nicaragua', 'Pakistan', 'Romania', 'Russia',
+          'Serbia', 'Singapore', 'Slovenia', 'Spain',
+          'Tunisia', 'Turkey', 'United States']
+
+estados = {}
+cidades = {}
+
+# Getting states
+for item in paises:
+    try:
+        r = requests.get(f"https://www.universal-tutorial.com/api/states/{item}", headers=headers)
+
+        retorno = r.json()
+        estados[item] = [i["state_name"].lower().replace(' ', '_') for i in retorno[:3]]
+    except:
+        print("ERROR")
+
+# Getting cities
+for estado_lista in estados.values():
+    for estado in estado_lista:
+        try:
+            r = requests.get(f"https://www.universal-tutorial.com/api/cities/{estado}", headers=headers)
+
+            retorno = r.json()
+            cidades[estado] = [i["city_name"].lower().replace(' ', '_') for i in retorno[:2]]
+        except:
+            print("ERROR")
+
+# Generating prolog database
+for item in paises:
+    print(f"country({item.lower().replace(' ', '_')}).")
+
+for pais, estados_lista in estados.items():
+    for estado in estados_lista:
+        print(f"state({pais.lower().replace(' ', '_')}, {estado.lower().replace(' ', '_')}).")
+
+for estado, cidades_lista in cidades.items():
+    for cidade in cidades_lista:
+        print(f"city({estado.lower().replace(' ', '_')}, {cidade.lower().replace(' ', '_')}).")
+
+for pais, estados_lista in estados.items():
+    for estado in estados_lista:
+        print(f"inside({pais.lower().replace(' ', '_')}, {estado.lower().replace(' ', '_')}).")
+
+for estado, cidades_lista in cidades.items():
+    for cidade in cidades_lista:
+        print(f"inside({estado.lower().replace(' ', '_')}, {cidade.lower().replace(' ', '_')}).")
